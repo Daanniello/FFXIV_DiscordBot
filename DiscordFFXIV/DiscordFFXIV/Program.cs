a15be4023a2546c6b83d2fd4166535d434f4740f
Daanniello <daansmits_@hotmail.com>
2018-11-22 11:12:38 +0100

    Setup

---
+++ DiscordFFXIV/DiscordFFXIV/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscordFFXIV
+{
+    class Program
+    {
+        public async Task Main(string[] args)
+            => Startup.RunAsync(args);
+    }
+}
